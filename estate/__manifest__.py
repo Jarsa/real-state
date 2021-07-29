ee33fd2feef76ba85318ee5f617d5e37f04bb683
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-07-29 10:03:04 -0500

    [ADD] estate: Add new module to repo

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021, Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Estate',
+    'summary': '',
+    'version': '13.0.1.0.0',
+    'category': 'Real Estte',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'Jarsa',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+}
