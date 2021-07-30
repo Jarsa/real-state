e58e64ae7d17853b6c851c0e91557e5957995a6e
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-07-30 10:06:59 -0500

    [IMP] estate: make fields name and expected price required

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,11 +8,15 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char()
+    name = fields.Char(
+        required=True,
+    )
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(
+        required=True,
+    )
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
