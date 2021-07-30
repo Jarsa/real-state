e94465de77611a98f6ebc74c699e534d1c53e69b
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-07-30 09:44:32 -0500

    [ADD] estate: create estate.property model with some fields

---
+++ estate/models/estate_property.py
@@ -0,0 +1,30 @@
+# Copyright 2021, Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+    )
