13150a1568921770cb90af7caf7f8f7352b48771
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-08-02 10:03:35 -0500

    [ADD] estate: add attributes to fields in estate.property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,12 +13,21 @@ class EstateProperty(models.Model):
     )
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.add(
+            fields.Date.today(), months=+3),
+    )
     expected_price = fields.Float(
         required=True,
     )
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(
+        readonly=True,
+        copy=False,
+    )
+    bedrooms = fields.Integer(
+        default=2,
+    )
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -31,4 +40,18 @@ class EstateProperty(models.Model):
             ('east', 'East'),
             ('west', 'West'),
         ],
+    )
+    active = fields.Boolean(
+        default=True,
+    )
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new',
+        copy=False,
     )
